b13e9136df1a4c21d0a88e8faa4727794d6ac75d
hossein <hossein13mousavi@gmail.com>
2022-04-25 12:21:51 +0430

    change project's structure and add tailwindCSS to the project

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  prefix: "",
+  important: true,
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+};
